53ec0a7e42d8255014df9546826a1e15c2639661
prem <thapaprem601@gmail.com>
2025-06-21 17:57:51 +0530

    Done

---
+++ Reverse_String.py
@@ -0,0 +1,10 @@
+# 4- Write a program in python print reverse string
+s=input("Enter the any values:")
+rev_str=""
+for char in s:
+    rev_str=char+rev_str
+print(rev_str)
+
+'''s=input("Enter the any values:")
+rev_str=s[::-1]
+print(rev_str)'''
